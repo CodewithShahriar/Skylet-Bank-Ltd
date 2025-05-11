4d87c642b054dc38d10222500b09ccc1b6e66582
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-11 14:29:15 +0600

    banking service digital account opeing side

--- src/components/BankingServices.tsx
+++ src/components/BankingServices.tsx
@@ -184,7 +184,7 @@ const BankingServices = () => {
                       className="w-full pr-10"
                     />
                     <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                      <Users className="h-5 w-5 text-bank-red" />
+                      <Users className="h-5 w-5 text-blue-600 text-gradient-green-500" />
                     </div>
                   </div>
                 </div>
@@ -199,7 +199,7 @@ const BankingServices = () => {
                       className="w-full pr-10"
                     />
                     <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-bank-red" viewBox="0 0 20 20" fill="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-700 text-gradient-green-500" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                       </svg>
@@ -216,17 +216,17 @@ const BankingServices = () => {
                       className="w-full pr-10"
                     />
                     <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-bank-red" viewBox="0 0 20 20" fill="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600 text-gradient-green-500" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                       </svg>
                     </div>
                   </div>
                 </div>
                 
-                <Button className="w-full bg-gray-100 text-gray-800 hover:bg-gray-200">
-                  Proceed to Details
-                  <ArrowRight size={16} className="ml-2" />
-                </Button>
+                <Button className="w-full bg-gradient-to-r from-blue-800 via-green-000 to-purple-500 text-white hover:bg-gradient-to-r hover:from-blue-600 hover:via-green-000 hover:to-purple-600">
+  Proceed to Details
+  <ArrowRight size={16} className="ml-2" />
+</Button>
               </div>
             </div>
           </div>
