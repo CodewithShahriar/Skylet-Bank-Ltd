8b16f667edda2de7e64183dfb2060fbb13c26b10
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-12 02:30:47 +0600

    apptsx import and route pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Accounts from "./pages/Accounts";
+import DepositsAndLoans from "./pages/DepositsAndLoans";
+import Cards from "./pages/Cards";
+import AboutUs from "./pages/AboutUs";
+import Support from "./pages/Support";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,6 +21,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/deposits-and-loans" element={<DepositsAndLoans />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/support" element={<Support />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
